8278c10d4c8a5665c3f79c510b6617406a8a33bf
Dallin24 <the4gates24@gmail.com>
2023-09-07 20:16:26 -0600

    Completed Part 2 of HW01

--- hw/hw01/admissions.py
+++ hw/hw01/admissions.py
@@ -4,6 +4,8 @@
 # Parameters:
 # row - a list to check
 # Returns True if the length of row is 8 and all elements are floats
+import csv
+
 def check_row_types(row):
     if len(row) != 8:
         print("Length incorrect! (should be 8): " + str(row))
@@ -26,6 +28,10 @@ def convert_row_type(old_list):
         index+=1
     return new_list
 
+def calculate_score(list):
+    score = 0.30 * (list[0] / 160) + 0.40 * (list[1] * 2) + 0.10 * (list[2]) + 0.20 * (list[3])
+    return score
+
 
 def main():
     # Change this line of code as needed but 
@@ -38,40 +44,58 @@ def main():
     print("Processing " + filename + "...")
     # grab the line with the headers
     headers = input_file.readline()
-   
-    
-    # TODO: loop through the rest of the file
     headers = headers[:-3]
     headers_splitted = headers.split(',')
     #print(headers_splitted)
-
+    
+    # TODO: loop through the rest of the file
     index = 1
-    while index <=214:
+    student_scores_list = []
+    chosen_students_list = []
+
+    while index <= 214:
         line = input_file.readline()
-        if index==214:
+
+        if index == 214:
             line = line
         else:
             line = line[:-1]
-        original_list = line.split(',')
 
-        student_name = [original_list[0]]
+        original_list = line.split(',')
+        student_name = original_list[0]
         newList = original_list[1:]
         floatList = convert_row_type(newList)
 
-        #print(student_name)
-        #print(original_list)
-        #print(newList)
-        #print(floatList)
         check_row_types(floatList)
 
         quality_list = floatList[0:4]
         semester_list = floatList[4:8]
 
+        student_score = [student_name, ("%.2f" % calculate_score(quality_list))]
+        student_scores_list.append(student_score)
+
+        if(float(student_score[1]) >= 6.00):
+            chosen_students_list.append(student_name + '\n')
+
+        #print(student_name)
+        #print(original_list)
+        #print(newList)
+        #print(floatList)
         #print(quality_list)
         #print(semester_list)
-
-
+        #print(scores)
         index+=1
+    
+    #print(student_scores)
+
+    student_scores_file = "hw/hw01/student_scores.csv"
+    with open(student_scores_file, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(student_scores_list)
+    
+    chosen_students_file = "hw/hw01/chosen_students.txt"
+    txtfile = open(chosen_students_file, 'w') 
+    txtfile.writelines(chosen_students_list)
 
     # TODO: make sure to close all files you've opened!
 
